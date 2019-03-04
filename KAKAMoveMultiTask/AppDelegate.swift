ff3c294ab1eb09b289df81994d918db268383d66
mu <muxq@wondershare.cn>
2019-03-04 17:54:19 +0800

    Init

---
+++ KAKAMoveMultiTask/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KAKAMoveMultiTask
+//
+//  Created by  on 2019/3/4.
+//  Copyright © 2019 kakamove. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
